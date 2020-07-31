9d8fc6d715879b197e9c8a4089e168e9009a3e71
Eddie Alvarez <eddieaugusto040@gmail.com>
2020-07-31 14:56:07 -0600

    client

---
+++ public/js/interaction.js
@@ -0,0 +1,58 @@
+//ALERTS
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true
+});
+
+
+//SELECT DOM ELEMENTS
+const btnDelete = document.querySelector('#btnDelete');
+const btnOpen = document.querySelector('#btnOpen');
+const fileInput = document.querySelector('#file-input');
+const btnSave = document.querySelector('#btnSave');
+
+//LISTENERS
+btnDelete.addEventListener('click', DeleteText);
+btnOpen.addEventListener('click', OpenTxt);
+fileInput.addEventListener('change', fileSelected);
+btnSave.addEventListener('click', saveText);
+
+//DELETE TEXT
+function DeleteText() {
+    document.querySelector('#txt').value = '';
+}
+
+//OPEN TXT
+function OpenTxt() {
+    fileInput.click();
+}
+
+//A FILE WAS SELECTED
+function fileSelected() {
+    const file = fileInput.files[0];
+    if (file.type == 'text/plain'){
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const content = reader.result;
+            document.querySelector('#txt').value = content;
+        }
+        reader.readAsText(file);
+    }else{
+        Toast.fire({
+            icon: 'error',
+            title: 'Solo se pueden abrir archivos con extension .txt'
+        });
+    } 
+}
+
+//SAVE TEXT
+function saveText() {
+    const content = document.querySelector('#txt').value;
+    const download = document.createElement('A');
+    download.href = `data:text/plain;charset=utf-8,${content}`
+    download.download = 'code.txt';
+    download.click();
+}
